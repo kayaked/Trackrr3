b6c319d6b5a5d8af723ca2d59c9d38063f15310f
exofeel <37344632+exofeel@users.noreply.github.com>
2018-12-24 16:28:34 -0500

    Added __slots__ for Song

--- cogs/mods/base.py
+++ cogs/mods/base.py
@@ -3,5 +3,10 @@ class Album:
 
     __slots__ = ('name', 'track_list', 'artist', 'link', 'release_date', 'cover_url', 'color', 'service')
 
+
+class Song:
+
+    __slots__ = {'name', 'artist', 'link', 'cover_url', 'link', 'release_date', 'track_album', 'color', 'service'}
+
 class NotFound(Exception):
     pass
