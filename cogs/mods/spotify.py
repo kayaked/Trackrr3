b98a06c931cd81827edf8242caf049f984729cb8
exofeel <37344632+exofeel@users.noreply.github.com>
2018-12-24 16:28:43 -0500

    Spotify now can search songs

--- cogs/mods/spotify.py
+++ cogs/mods/spotify.py
@@ -11,6 +11,8 @@ from datetime import datetime
 
 class NotFound(Exception):
     pass
+class SpotifyError(Exception):
+    pass
 
 async def authorize_spotify():
     """ Authoirzies Spotify using PyFy's Method """
@@ -20,6 +22,91 @@ async def authorize_spotify():
     await spotify.authorize_client_creds()
     return spotify
 
+
+async def search_song(song_name):
+    spotify = await authorize_spotify()
+    try:
+        spotify_search_query = spotify.search(song_name, types='track')
+        track_selected = spotify_search_query['tracks']['items'][0]
+    except IndexError:
+        raise NotFound("No Results found for spotify")
+    except Exception:
+        raise NotFound("Error!")
+    
+    async def track_artist():
+        try:
+            return track_selected['artists'][0]['name']
+        except IndexError:
+            raise NotFound("No results for spotify")
+        except Exception as err:
+            print(err)
+            return "Unknown!"
+
+    async def track_name():
+        try:
+            return track_selected['name']
+        except IndexError:
+            raise NotFound("No results for spotify")
+        except Exception as err:
+            print(err)
+            return "Unknown!"
+    
+    async def get_url():
+        try:
+            return track_selected['external_urls']['spotify']
+        except IndexError:
+            raise NotFound("No results for spotify")
+        except Exception as err:
+            print(err)
+            return "Unknown!"
+
+    async def get_cover_art():
+        try:
+            return track_selected['album']['images'][0]['url']
+        except IndexError:
+            raise NotFound("No results for spotify")
+        except Exception as err:
+            print(err)
+            return "Unknown!"
+    
+    async def track_album():
+        try:
+            return track_selected['album']['name']
+        except IndexError:
+            raise NotFound("No results for spotify")
+        except Exception as err:
+            print(err)
+            return "Unknown!"
+   
+    # The date the album was first released, for example “1981-12-15”. 
+    # Depending on the precision, it might be shown as “1981” or “1981-12”.
+    # The precision with which release_date value is known: “year” , “month” , or “day”.	
+    async def get_release_date():
+        album_selected = track_selected['album']
+        album_release_date = album_selected['release_date']
+        album_release_date_type = album_selected['release_date_precision']
+        try:
+            if album_release_date_type == "day":
+               parsed_time = datetime.strptime(album_release_date, "%Y-%m-%d")
+               return parsed_time
+            elif album_release_date_type == "month":
+                parsed_time = datetime.strptime(album_release_date, "%Y-%m")
+                return parsed_time
+            elif parsed_time == "year":
+                return datetime.strptime(album_release_date, "%Y")
+        except Exception:
+            return "Unknown!"
+
+    info = {
+        "TrackName": await track_name(),
+        "TrackArtist": await track_artist(),
+        "TrackCoverArt": await get_cover_art(),
+        "TrackAlbum": await track_album(),
+        "TrackReleaseDate": await get_release_date(),
+        "TrackURL": await get_url()
+    }
+
+    return SpotifySong(info)
 
 
 async def search_album(album_name):
@@ -32,7 +119,7 @@ async def search_album(album_name):
     except Exception as error:
         return error
 
-
+    
 
     async def get_album_name():
         try:
@@ -108,3 +195,15 @@ class SpotifyAlbum(Album):
         self.track_list = data['TrackList']
         self.cover_url = data['AlbumCoverArt']
         self.release_date = data['ReleaseDate']
+
+class SpotifySong(Song):
+
+    def __init__(self, data:dict):
+        self.color = 0x84bd00
+        self.service = 'Spotify'
+        self.name = data['TrackName']
+        self.artist = data['TrackArtist']
+        self.link = data['TrackURL']
+        self.cover_url = data['TrackCoverArt']
+        self.track_album = data['TrackAlbum']
+        self.release_date = data['TrackReleaseDate']
