09e98a39f16b57818635af03d99c690041334741
exofeel <37344632+exofeel@users.noreply.github.com>
2018-12-30 20:49:41 -0500

    removed unneeded classes

--- cogs/mods/base.py
+++ cogs/mods/base.py
@@ -9,11 +9,5 @@ class Song:
     __slots__ = {'name', 'artist', 'link', 'cover_url', 'release_date', 'track_album', 'color', 'service'}
 
 
-# For Spotify Discord
-
-class SpotifyDiscordSong:
-
-	__slots__ = {'track_name', 'track_artist', 'track_album', 'track_id'}
-
 class NotFound(Exception):
     pass
